f8f83cf5d748c835cf29ebcbdf54e39c21ea5008
drags44 <dragmireshadow9@gmail.com>
2024-08-01 16:49:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// Example of a simple alert on page load
-window.onload = function() {
-    alert('Welcome to My Boy Scouts Page!');
-};
+// // Example of a simple alert on page load
+// window.onload = function() {
+//     alert('Welcome to My Boy Scouts Page!');
+// };
 
-// Example function to validate a contact form
-function validateForm() {
-    var name = document.forms["contactForm"]["name"].value;
-    var email = document.forms["contactForm"]["email"].value;
-    if (name == "" || email == "") {
-        alert("Name and email must be filled out");
-        return false;
-    }
-}
+// // Example function to validate a contact form
+// function validateForm() {
+//     var name = document.forms["contactForm"]["name"].value;
+//     var email = document.forms["contactForm"]["email"].value;
+//     if (name == "" || email == "") {
+//         alert("Name and email must be filled out");
+//         return false;
+//     }
+// }
